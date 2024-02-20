43643fd067d48c1fd4ccf06484d292984fe05249
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-02-20 01:29:01 -0500

    some base styling up

--- app/views/root.py
+++ app/views/root.py
@@ -2,5 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 
+# title = {
+#     'title': '',
+#     'header': 'BeeDev Services',
+# }
+
 def index(request):
-    return redirect('/admin/')
+    title = {
+        'title': 'Home',
+        'header': 'TechByte Learning',
+    }
+    context = {
+        'title': title,
+    }
+    return render(request, 'index.html', context)
